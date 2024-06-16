cac4abe9775a0ce2a5c17bd8fe3e5e876953b45a
Jack Kendall <jkendall3096@gmail.com>
2024-06-16 13:01:51 -0700

    Clean up Program.cs

--- HydrusReplacement.Server/Program.cs
+++ HydrusReplacement.Server/Program.cs
@@ -3,16 +3,12 @@ using HydrusReplacement.Server.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddDbContext<ServerDbContext>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -20,7 +16,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.AddEndpoints();
 
 app.Run();
