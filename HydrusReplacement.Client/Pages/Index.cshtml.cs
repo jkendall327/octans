20401120fdc09b0bfedbaa40e34c30a30dbca067
Jack Kendall <jkendall3096@gmail.com>
2024-06-17 19:43:51 -0700

    Display filepaths instead of raw hashes

--- HydrusReplacement.Client/Pages/Index.cshtml.cs
+++ HydrusReplacement.Client/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using HydrusReplacement.Server;
 using HydrusReplacement.Server.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -6,20 +7,30 @@ namespace HydrusReplacement.Client.Pages;
 
 public class IndexModel : PageModel
 {
-    public List<HashItem> Hashes { get; set; } = new();
-    
+    public List<Uri> Uris { get; set; } = new();
+
+    private readonly SubfolderManager _subfolderManager;
+
+    public IndexModel(SubfolderManager subfolderManager)
+    {
+        _subfolderManager = subfolderManager;
+    }
+
     public async Task<IActionResult> OnGetAsync()
     {
         var client = new HttpClient();
 
+        // TODO: don't hardcode this
         var response = await client.GetFromJsonAsync<List<HashItem>>("http://localhost:5185/getAll");
 
         if (response is null)
         {
             return NotFound();
         }
-        
-        Hashes = response;
+
+        Uris = response
+            .Select(x => _subfolderManager.GetSubfolder(x.Hash))
+            .ToList();
 
         return Page();
     }
