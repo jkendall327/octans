2081042133d3ece827c2fda66e477f3f784d43c7
jackkendall <jkendall3096@gmail.com>
2024-06-30 16:49:14 +0100

    Remove comments

--- HydrusReplacement.Client/Program.cs
+++ HydrusReplacement.Client/Program.cs
@@ -2,27 +2,21 @@ using HydrusReplacement.Server;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddHttpClient();
-
 builder.Services.AddScoped<SubfolderManager>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapRazorPages();
