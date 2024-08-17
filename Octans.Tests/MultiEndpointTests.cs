55da067c038bbfd431b9e1b238d3e4a1711dcb06
jackkendall <jkendall3096@gmail.com>
2024-08-17 15:36:25 +0100

    Clean up code for the E2E test and add condition for not removing mappings

--- Octans.Tests/MultiEndpointTests.cs
+++ Octans.Tests/MultiEndpointTests.cs
@@ -1,11 +1,9 @@
-using System.IO.Abstractions.TestingHelpers;
 using System.Net.Http.Json;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Octans.Core;
 using Octans.Core.Importing;
-using Octans.Core.Models;
 
 namespace Octans.Tests;
 
@@ -16,39 +14,58 @@ public class MultiEndpointIntegrationTests : EndpointTest
     [Fact]
     public async Task ImportUpdateAndDeleteImage_ShouldSucceed()
     {
-        // Arrange
-        var client = _factory.CreateClient();
+        // Add a test file.
         var imagePath = "C:/test_image.jpg";
-        _fileSystem.AddFile(imagePath, new MockFileData(TestingConstants.MinimalJpeg));
+        _fileSystem.AddFile(imagePath, new(TestingConstants.MinimalJpeg));
 
-        // Import
-        var importRequest = new ImportRequest
-        {
-            Items =
-            [
-                new()
-                {
-                    Source = new Uri(imagePath),
-                    Tags =
-                    [
-                        new() { Namespace = "category", Subtag = "test" }
-                    ]
-                }
-            ],
-            DeleteAfterImport = false
-        };
+        var expectedFilePath = _fileSystem.Path.Join(_appRoot,
+            "db",
+            "files",
+            "f61",
+            "61F461B34DCF8D8227A8691A6625444C1E2C793A181C7D0AD5EF8B15D5E6D040.jpg");
+        
+        await ImportFile(imagePath, expectedFilePath);
 
-        var importResponse = await client.PostAsJsonAsync("/import", importRequest);
-        importResponse.EnsureSuccessStatusCode();
-        var importResult = await importResponse.Content.ReadFromJsonAsync<ImportResult>();
-        importResult.Should().NotBeNull();
-        importResult!.Results.Single().Ok.Should().BeTrue();
-
-        // Get the HashId
         var hashItem = await _context.Hashes.SingleAsync();
         var hashId = hashItem.Id;
 
-        // Update Tags
+        await UpdateTags(hashId);
+
+        await DeleteItem(hashId, expectedFilePath);
+    }
+
+    private async Task ImportFile(string imagePath, string expectedFilePath)
+    {
+        var client = _factory.CreateClient();
+
+        var item = new ImportItem
+        {
+            Source = new(imagePath),
+            Tags = [ new() { Namespace = "category", Subtag = "test" } ]
+        };
+        
+        var request = new ImportRequest
+        {
+            Items = [item],
+            DeleteAfterImport = false
+        };
+
+        var response = await client.PostAsJsonAsync("/import", request);
+        
+        response.EnsureSuccessStatusCode();
+        
+        var result = await response.Content.ReadFromJsonAsync<ImportResult>();
+        
+        result.Should().NotBeNull();
+        result!.Results.Single().Ok.Should().BeTrue("this import has no reason to fail");
+        
+        _fileSystem.FileExists(expectedFilePath).Should().BeTrue("we write the bytes to the hex bucket on import");
+    }
+    
+    private async Task UpdateTags(int hashId)
+    {
+        var client = _factory.CreateClient();
+
         var updateTagsRequest = new UpdateTagsRequest
         {
             HashId = hashId,
@@ -57,33 +74,60 @@ public class MultiEndpointIntegrationTests : EndpointTest
         };
 
         var updateResponse = await client.PutAsJsonAsync("/updateTags", updateTagsRequest);
+        
         updateResponse.EnsureSuccessStatusCode();
 
-        // Verify tag update
-        var updatedTags = await _context.Mappings
+        var tags = await _context.Mappings
             .Where(m => m.Hash.Id == hashId)
             .Select(m => new { Namespace = m.Tag.Namespace.Value, Subtag = m.Tag.Subtag.Value })
             .ToListAsync();
 
-        updatedTags.Should().ContainSingle(t => t.Namespace == "character" && t.Subtag == "mario");
-        updatedTags.Should().NotContain(t => t.Namespace == "category" && t.Subtag == "test");
+        tags
+            .Should()
+            .ContainSingle(t => t.Namespace == "character" && t.Subtag == "mario", 
+                "we should have added this tag/mapping");
+        
+        tags
+            .Should()
+            .NotContain(t => t.Namespace == "category" && t.Subtag == "test", 
+                "we should have removed this tag/mapping");
+    }
+    
+    private async Task DeleteItem(int hashId, string expectedFilepath)
+    {
+        var client = _factory.CreateClient();
 
-        // Delete
-        var deleteRequest = new DeleteRequest([new(hashId)]);
-        var deleteResponse = await client.PostAsJsonAsync("/delete", deleteRequest);
-        deleteResponse.EnsureSuccessStatusCode();
-        var deleteResult = await deleteResponse.Content.ReadFromJsonAsync<DeleteResponse>();
-        deleteResult.Should().NotBeNull();
-        deleteResult!.Results.Single().Success.Should().BeTrue();
+        var request = new DeleteRequest([new(hashId)]);
 
-        // Verify deletion
-        var deletedHash = await _context.Hashes.FindAsync(hashId);
-        deletedHash.Should().NotBeNull();
-        await _context.Entry(deletedHash!).ReloadAsync();
-        deletedHash!.DeletedAt.Should().NotBeNull();
+        var mappings = await _context.Mappings.Where(m => m.Hash.Id == hashId).ToListAsync();
+        
+        var response = await client.PostAsJsonAsync("/delete", request);
+        
+        response.EnsureSuccessStatusCode();
+        
+        var result = await response.Content.ReadFromJsonAsync<DeleteResponse>();
+        
+        result.Should().NotBeNull();
+        result!.Results.Single().Success.Should().BeTrue();
 
-        // Verify file removal
-        var expectedFilePath = _fileSystem.Path.Join(_appRoot, "db", "files", "f61", "61F461B34DCF8D8227A8691A6625444C1E2C793A181C7D0AD5EF8B15D5E6D040.jpg");
-        _fileSystem.FileExists(expectedFilePath).Should().BeFalse();
+        // Verify deletion in database
+        // We have to reload the item so EF doesn't give us the version in its cache
+        // which doesn't reflect the SUT setting the DeletedAt flag.
+        var hash = await _context.Hashes.FindAsync(hashId);
+        await _context.Entry(hash!).ReloadAsync();
+        
+        hash.Should().NotBeNull("we soft-delete hashes to prevent them being reimported later");
+        hash!.DeletedAt.Should().NotBeNull("we soft-delete items by setting this value to something non-null");
+
+        // Verify removal from filesystem
+        _fileSystem.FileExists(expectedFilepath)
+            .Should()
+            .BeFalse("we remove the physical file even for soft-deletes");
+        
+        var mappingsAfterDeletion = await _context.Mappings
+            .Where(m => m.Hash.Id == hashId)
+            .ToListAsync();
+
+        mappingsAfterDeletion.Should().BeEquivalentTo(mappings, "we don't remove mappings for deleted items");
     }
 }
