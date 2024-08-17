4ff72170aa8590c7bfc4c98aca923c9f8be5ebf8
jackkendall <jkendall3096@gmail.com>
2024-08-17 15:14:19 +0100

    Get integration test passing

--- Octans.Server/Services/TagUpdater.cs
+++ Octans.Server/Services/TagUpdater.cs
@@ -21,7 +21,7 @@ public class TagUpdater
             return false;
         }
 
-        RemoveTags(request.TagsToRemove, hash);
+        await RemoveTags(request.TagsToRemove, hash);
         await AddTags(request.TagsToAdd, hash);
 
         await _context.SaveChangesAsync();
@@ -29,7 +29,7 @@ public class TagUpdater
         return true;
     }
 
-    private void RemoveTags(IEnumerable<TagModel> tagsToRemove, HashItem hash)
+    private async Task RemoveTags(IEnumerable<TagModel> tagsToRemove, HashItem hash)
     {
         var all = _context.Mappings
             .Include(m => m.Tag)
@@ -37,11 +37,21 @@ public class TagUpdater
             .Include(m => m.Tag)
             .ThenInclude(t => t.Subtag);
 
-        var forThisHash = all.Where(m => m.Hash.Id == hash.Id);
-
-        var mappingsToRemove = forThisHash.Where(m => tagsToRemove.Any(t =>
-                ((t.Namespace == null && m.Tag.Namespace.Value == "") || (t.Namespace != null && m.Tag.Namespace.Value == t.Namespace)) 
-                && m.Tag.Subtag.Value == t.Subtag));
+        // TODO: this is probably bad when we have lots of mappings for a given hash.
+        // But trying to do it against an IQueryable makes EF Core explode as it can't translate it.
+        var forThisHash = await all.Where(m => m.Hash.Id == hash.Id).ToListAsync();
+        
+        var mappingsToRemove = forThisHash.Where(m =>
+        {
+            return tagsToRemove.Any(t =>
+            {
+                var namespacesMatch = 
+                    (t.Namespace == null && m.Tag.Namespace.Value == "") ||
+                    (t.Namespace != null && m.Tag.Namespace.Value == t.Namespace);
+                
+                return namespacesMatch && m.Tag.Subtag.Value == t.Subtag;
+            });
+        });
         
         _context.Mappings.RemoveRange(mappingsToRemove);
     }
@@ -63,13 +73,22 @@ public class TagUpdater
 
             if (tag == null)
             {
-                tag = new Tag { Namespace = @namespace, Subtag = subtag };
+                tag = new()
+                {
+                    Namespace = @namespace, Subtag = subtag
+                };
+                
                 _context.Tags.Add(tag);
             }
 
-            if (!await _context.Mappings.AnyAsync(m => m.Hash == hash && m.Tag == tag))
+            var exists = await _context.Mappings.AnyAsync(m => m.Hash == hash && m.Tag == tag);
+            
+            if (!exists)
             {
-                _context.Mappings.Add(new Mapping { Hash = hash, Tag = tag });
+                _context.Mappings.Add(new()
+                {
+                    Hash = hash, Tag = tag
+                });
             }
         }
     }
