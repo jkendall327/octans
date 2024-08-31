839c3f60529c50949088695e34ceba20af9a683e
jackkendall <jkendall3096@gmail.com>
2024-08-31 11:43:08 +0100

    Ensure test DB and filesystem are used in tests

--- Octans.Tests/Helpers/EndpointTest.cs
+++ Octans.Tests/Helpers/EndpointTest.cs
@@ -18,22 +18,50 @@ namespace Octans.Tests;
 public class EndpointTest : IClassFixture<WebApplicationFactory<Program>>, IAsyncLifetime
 {
     protected readonly WebApplicationFactory<Program> _factory;
+    
+    // Database
     private readonly SqliteConnection _connection = new("DataSource=:memory:");
-    protected readonly MockFileSystem _fileSystem = new();
     protected ServerDbContext _context = null!;
+    
+    // Filesystem
     protected readonly string _appRoot = "C:/app";
+    protected readonly MockFileSystem _fileSystem = new();
+    
+    // Channels
     protected readonly SpyChannelWriter<ThumbnailCreationRequest> _spyChannel = new();
 
+    /// <summary>
+    /// Checks we've correctly set things up with fakes rather than the real DB, filesystem etc.
+    /// </summary>
     public async Task InitializeAsync()
     {
         await _connection.OpenAsync();
         
         _context = _factory.Services.CreateScope().ServiceProvider.GetRequiredService<ServerDbContext>();
 
+        var connectionString = _context.Database.GetConnectionString();
+        
+        if (connectionString != "DataSource=:memory:")
+        {
+            throw new InvalidOperationException(
+                "The in-memory database wasn't set up for tests (check the service replacement code)");
+        }
+        
         await _context.Database.EnsureCreatedAsync();
+
+        var filesystem = _factory.Services.GetRequiredService<IFileSystem>();
+
+        if (filesystem is FileSystem)
+        {
+            throw new InvalidOperationException("Test system was set up with a real filesystem, not a mock one (check the service replacement code)");
+        }
     }
 
-    public async Task DisposeAsync() => await _connection.DisposeAsync();
+    public async Task DisposeAsync()
+    {
+        await _context.DisposeAsync();
+        await _connection.DisposeAsync();
+    }
 
     protected EndpointTest(WebApplicationFactory<Program> factory)
     {
