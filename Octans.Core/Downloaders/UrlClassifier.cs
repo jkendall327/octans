5f029504aa0c61ff3e466fc31649674d49aa5cbe
jackkendall <jkendall3096@gmail.com>
2024-09-21 14:14:02 +0100

    Begin sketching out URL classifier system

--- Octans.Core/Downloaders/UrlClassifier.cs
+++ Octans.Core/Downloaders/UrlClassifier.cs
@@ -1,24 +1,106 @@
+using System.IO.Abstractions;
 using NLua;
 
 namespace Octans.Core.Downloaders;
 
 public class UrlClassifier
 {
-    public async Task<bool> Matches(string url)
+    private readonly DownloaderFactory _factory;
+
+    public UrlClassifier(DownloaderFactory factory)
     {
-        var lua = new Lua();
+        _factory = factory;
+    }
 
-        var raw = """
-                  function match_url(url)
-                      return string.find(url, "twitter%.com") ~= nil
-                  end
-                  """;
+    public async Task<Downloader?> Matches(string url)
+    {
+        var downloaders = await _factory.GetDownloaders();
         
-        lua.DoString(raw);
-        
-        var scriptFunc = lua ["match_url"] as LuaFunction;
-        var res = scriptFunc?.Call(url)?.First();
+        foreach (var downloader in downloaders)
+        {
+            var matches = downloader.MatchesUrl(url);
 
-        return (bool) res;
+            if (matches)
+            {
+                return downloader;
+            }
+        }
+
+        return null;
+    }
+}
+
+public class DownloaderFactory
+{
+    private readonly IFileSystem _fileSystem;
+    private readonly GlobalSettings _globalSettings;
+
+    public async Task<List<Downloader>> GetDownloaders()
+    {
+        var downloaders = _fileSystem.Path.Join(_globalSettings.AppRoot, "downloaders");
+        var dirs = _fileSystem.DirectoryInfo.New(downloaders);
+
+        var d = new List<Downloader>();
+        
+        foreach (var subdir in dirs.EnumerateDirectories())
+        {
+            var files = subdir.EnumerateFiles("*.lua", SearchOption.TopDirectoryOnly).ToList();
+
+            var downloader = await Create(files);
+
+            d.Add(downloader);
+        }
+
+        return d;
+    }
+
+    private async Task<Downloader> Create(List<IFileInfo> sources)
+    {
+        string[] names = ["metadata", "classifier", "parser", "gug", "api"];
+
+        var functions = new Dictionary<string, Lua>();
+        
+        foreach (var name in names)
+        {
+            var file = sources.SingleOrDefault(s => string.Equals(s.Name, name, StringComparison.InvariantCultureIgnoreCase));
+
+            if (file is null) continue;
+            
+            var raw = await File.ReadAllTextAsync(file.FullName);
+            
+            var lua = new Lua();
+
+            lua.DoString(raw);
+            
+            functions.Add(name, lua);
+        }
+
+        return new(functions);
+    }
+}
+
+public class Downloader
+{
+    private readonly Dictionary<string, Lua> _functions;
+
+    private readonly LuaFunction _matchUrl;
+    
+    public Downloader(Dictionary<string, Lua> functions)
+    {
+        _functions = functions;
+        
+        _matchUrl = _functions["classifier"]["match_url"] as LuaFunction ?? throw new InvalidOperationException();
+    }
+
+    public bool MatchesUrl(string url)
+    {
+        var res = _matchUrl.Call(url)?.First();
+
+        if (res is bool b)
+        {
+            return b;
+        }
+
+        throw new InvalidOperationException("Bad script");
     }
 }
