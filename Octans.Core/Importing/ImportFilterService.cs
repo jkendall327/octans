7ed772b619252f915fc6c7de7da6b6dd10a68c87
jackkendall <jkendall3096@gmail.com>
2024-12-09 20:14:51 +0000

    Organise core project

--- Octans.Core/Importing/ImportFilterService.cs
+++ Octans.Core/Importing/ImportFilterService.cs
@@ -3,15 +3,8 @@ using Octans.Core.Importing;
 
 namespace Octans.Server;
 
-public sealed class ImportFilterService
+public sealed class ImportFilterService(ILogger<ImportFilterService> logger)
 {
-    private readonly ILogger<ImportFilterService> _logger;
-
-    public ImportFilterService(ILogger<ImportFilterService> logger)
-    {
-        _logger = logger;
-    }
-
     public async Task<ImportItemResult?> ApplyFilters(ImportRequest request, byte[] bytes)
     {
         if (request.FilterData is null)
@@ -30,11 +23,11 @@ public sealed class ImportFilterService
         {
             var result = await filter.PassesFilter(request.FilterData, bytes);
 
-            _logger.LogDebug("{FilterName} result: {FilterResult}", filter.GetType().Name, result);
+            logger.LogDebug("{FilterName} result: {FilterResult}", filter.GetType().Name, result);
 
             if (result) continue;
 
-            _logger.LogInformation("File rejected by import filters");
+            logger.LogInformation("File rejected by import filters");
 
             return new()
             {
--- Octans.Core/Importing/ImportModels.cs
+++ Octans.Core/Importing/ImportModels.cs
@@ -41,3 +41,17 @@ public class ImportItem
     public required Uri Source { get; init; }
     public ICollection<TagModel>? Tags { get; init; }
 }
+
+/// <summary>
+/// Represents the success of the import for an individual item/file.
+/// </summary>
+public class ImportItemResult
+{
+    public required bool Ok { get; set; }
+    public string? Message { get; set; }
+}
+
+/// <summary>
+/// Represents the success of an overall import, which could include multiple items.
+/// </summary>
+public record ImportResult(Guid ImportId, List<ImportItemResult> Results);
