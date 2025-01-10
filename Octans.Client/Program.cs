218d2c8046c1b2f8a416486df3df579bf7f9c79d
jackkendall <jkendall3096@gmail.com>
2025-01-10 17:33:54 +0000

    Move over types

--- Octans.Client/Program.cs
+++ Octans.Client/Program.cs
@@ -1,24 +1,39 @@
+using System.IO.Abstractions;
+using Octans.Client;
 using Octans.Client.Components;
+using Octans.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
+builder.Services.AddHttpClient();
+
+builder.Services.AddScoped<SubfolderManager>();
+builder.Services.AddSingleton<IFileSystem>(new FileSystem());
+builder.Services.AddSingleton<ImportRequestSender>();
+
+builder.Services.Configure<GlobalSettings>(builder.Configuration.GetSection("GlobalSettings"));
+
+builder.Services.AddHttpClient<ServerClient>(client =>
+{
+    var port = CommunicationConstants.OCTANS_SERVER_PORT;
+    client.BaseAddress = new($"http://localhost:{port}/");
+});
+
+builder.Services.AddHostedService<ImportFolderBackgroundService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 
