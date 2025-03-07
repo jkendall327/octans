5d424cdb0a0c4de7e136887de878c4896b0ea78e
jackkendall <jkendall3096@gmail.com>
2025-03-07 20:24:49 +0000

    Begin trying to integrate into actual app

--- Octans.Core/Downloads/DownloadService.cs
+++ Octans.Core/Downloads/DownloadService.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Octans.Core.Downloaders;
 
 namespace Octans.Core.Downloads;
@@ -9,6 +11,37 @@ public interface IDownloadService
     Task PauseDownloadAsync(Guid id);
     Task ResumeDownloadAsync(Guid id);
     Task RetryDownloadAsync(Guid id);
+}
+
+public class DownloadManagerOptions
+{
+    public int MaxConcurrentDownloads { get; set; } = 3;
+}
+
+public static class DownloadServiceExtensions
+{
+    public static IServiceCollection AddDownloadManager(
+        this IServiceCollection services, 
+        Action<DownloadManagerOptions>? configure = null)
+    {
+        // Add options
+        var options = new DownloadManagerOptions();
+        configure?.Invoke(options);
+        services.AddSingleton(options);
+        
+        services.AddSingleton<DownloadStateService>();
+        services.AddScoped<IDownloadService, DownloadService>();
+        services.AddSingleton<IDownloadQueue, DatabaseDownloadQueue>();
+        
+        // Add HTTP client
+        services.AddHttpClient("DownloadClient", client => 
+        {
+            // Configure default headers, etc.
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("MyApp/1.0");
+        });
+        
+        return services;
+    }
 }
 
 public class DownloadService : IDownloadService
--- Octans.Server/Program.cs
+++ Octans.Server/Program.cs
@@ -1,6 +1,7 @@
 using System.Text.Json.Serialization;
 using Octans.Core;
 using Octans.Core.Communication;
+using Octans.Core.Downloads;
 using Octans.Core.Models;
 using Octans.Server;
 
@@ -32,6 +33,16 @@ builder.AddChannels();
 builder.AddDatabase();
 builder.AddBusinessServices();
 
+builder.Services.AddBandwidthLimiter(options => {
+    options.DefaultBytesPerSecond = 1024 * 1024; // 1 MB/s
+});
+
+builder.Services.AddDownloadManager(options => {
+    options.MaxConcurrentDownloads = 5;
+});
+
+builder.Services.AddHostedService<DownloadManager>();
+
 builder.Services.AddHostedService<ThumbnailCreationBackgroundService>();
 
 var app = builder.Build();
@@ -44,6 +55,15 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.AddEndpoints();
+
+var s = app.Services.GetRequiredService<DownloadService>();
+
+await s.QueueDownloadAsync(new() {
+    Url = "https://example.com/file.zip",
+    DestinationPath = "C:/Downloads/file.zip"
+});
+
+return;
 
 // Ensure subfolders are initialised.
 var manager = app.Services.GetRequiredService<SubfolderManager>();
