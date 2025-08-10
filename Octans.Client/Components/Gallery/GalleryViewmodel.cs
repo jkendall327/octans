db519730d559299292d4ed4879058eec8ea81b9f
jackkendall <jkendall3096@gmail.com>
2025-08-10 17:41:06 +0100

    Notifying state changes

--- Octans.Client/Components/Gallery/GalleryViewmodel.cs
+++ Octans.Client/Components/Gallery/GalleryViewmodel.cs
@@ -1,46 +1,74 @@
-using System.IO.Abstractions;
 using Octans.Core;
 using Octans.Core.Querying;
 
 namespace Octans.Client.Components.Pages;
 
-public sealed class GalleryViewmodel(QueryService service, SubfolderManager manager) : IAsyncDisposable
+public sealed class GalleryViewmodel(QueryService service, SubfolderManager manager, ILogger<GalleryViewmodel> logger)
+    : IAsyncDisposable
 {
     private CancellationTokenSource _cts = new();
-    
+
     public List<string> ImagePaths { get; private set; } = [];
     public bool Searching { get; private set; }
+    public string? LastError { get; private set; }
+    public Func<Task>? StateChanged { get; set; }
 
     public async Task OnQueryChanged(List<QueryParameter> arg)
     {
+        LastError = null;
         Searching = true;
+        ImagePaths = [];
+
+        await NotifyStateChanged();
 
         try
         {
+            var raw = arg.Select(s => s.Raw);
 
-            var results = await service.Query(arg.Select(s => s.Raw));
+            var results = service.Query(raw, _cts.Token);
 
-            var paths = results
-                .Select(s => HashedBytes.FromUnhashed(s.Hash))
-                .Select(manager.GetFilepath)
-                .OfType<IFileSystemInfo>()
-                .Select(x => x.FullName)
-                .ToList();
+            var i = 0;
 
-            ImagePaths = paths;
+            await foreach (var result in results)
+            {
+                var info = manager.GetFilepath(HashedBytes.FromUnhashed(result.Hash));
+
+                if (info is null)
+                {
+                    continue;
+                }
+
+                ImagePaths.Add(info.FullName);
+
+                i++;
+
+                if (i >= 5)
+                {
+                    await NotifyStateChanged();
+                    i = 0;
+                }
+            }
         }
         catch (Exception e)
         {
-            Console.WriteLine(e);
-
-            throw;
+            logger.LogError(e, "Unexpected exception during query");
+            LastError = e.Message;
         }
         finally
         {
             Searching = false;
+            await NotifyStateChanged();
         }
     }
-    
+
+    private async Task NotifyStateChanged()
+    {
+        if (StateChanged is not null)
+        {
+            await StateChanged.Invoke();
+        }
+    }
+
     public async Task OnCancel()
     {
         await _cts.CancelAsync();
--- Octans.Core/Querying/QueryService.cs
+++ Octans.Core/Querying/QueryService.cs
@@ -1,10 +1,12 @@
+using System.Runtime.CompilerServices;
 using Octans.Core.Models;
 
 namespace Octans.Core.Querying;
 
 public class QueryService(QueryParser parser, QueryPlanner planner, QueryTagConverter converter, HashSearcher searcher)
 {
-    public async Task<HashSet<HashItem>> Query(IEnumerable<string> queries)
+    public async IAsyncEnumerable<HashItem> Query(IEnumerable<string> queries,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
         var predicates = parser.Parse(queries);
 
@@ -12,8 +14,11 @@ public class QueryService(QueryParser parser, QueryPlanner planner, QueryTagConv
 
         var query = converter.Reduce(plan);
 
-        var items = await searcher.Search(query);
+        var items = await searcher.Search(query, cancellationToken);
 
-        return items;
+        foreach (var item in items)
+        {
+            yield return item;
+        }
     }
 }
