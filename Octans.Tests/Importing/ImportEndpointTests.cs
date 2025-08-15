7e86968cedd7b25fba5d26b51e3d86b454e81a55
jackkendall <jkendall3096@gmail.com>
2025-08-15 15:32:18 +0100

    All tests passing again

--- Octans.Tests/Importing/ImportEndpointTests.cs
+++ Octans.Tests/Importing/ImportEndpointTests.cs
@@ -1,5 +1,6 @@
 using System.IO.Abstractions;
 using System.IO.Abstractions.TestingHelpers;
+using System.Threading.Channels;
 using Octans.Core.Importing;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
@@ -42,8 +43,7 @@ public sealed class ImporterTests : IAsyncLifetime
 
         services.AddSingleton<IFileSystem>(_fileSystem);
 
-        services.AddSingleton(_spy.Channel.Writer);
-        services.AddSingleton(_spy.Channel.Reader);
+        services.AddSingleton<ChannelWriter<ThumbnailCreationRequest>>(_spy);
 
         services.AddHttpClient();
 
@@ -62,6 +62,59 @@ public sealed class ImporterTests : IAsyncLifetime
         result.Should().NotBeNull();
         result.ImportId.Should().Be(request.ImportId);
         result.Results.Single().Ok.Should().BeTrue();
+    }
+    
+    [Fact]
+    public async Task Import_ValidRequest_WritesFileToSubfolder()
+    {
+        _ = await SendSimpleValidRequest();
+    
+        var expectedPath = _fileSystem.Path.Join(AppRoot,
+            "db",
+            "files",
+            "f61",
+            "61F461B34DCF8D8227A8691A6625444C1E2C793A181C7D0AD5EF8B15D5E6D040.jpg");
+    
+        var file = _fileSystem.GetFile(expectedPath);
+    
+        file.Should().NotBeNull();
+    }
+    
+    [Fact]
+    public async Task Import_ValidRequest_PersistsInfoToDatabase()
+    {
+        _ = await SendSimpleValidRequest();
+
+        using var serviceScope = _provider.CreateScope();
+        
+        var db = serviceScope.ServiceProvider.GetRequiredService<ServerDbContext>();
+        
+        var mapping = db.Mappings
+            .Include(mapping => mapping.Hash)
+            .Include(mapping => mapping.Tag)
+            .ThenInclude(tag => tag.Namespace)
+            .Include(mapping => mapping.Tag)
+            .ThenInclude(tag => tag.Subtag)
+            .Single();
+    
+        mapping.Tag.Namespace.Value.Should().Be("category", "the namespace should be linked the tag");
+        mapping.Tag.Subtag.Value.Should().Be("example", "the subtag should be linked to the tag");
+    
+        var hashed = HashedBytes.FromUnhashed(TestingConstants.MinimalJpeg);
+    
+        mapping.Hash.Hash.Should().BeEquivalentTo(hashed.Bytes, "we should be persisting the hashed bytes");
+    }
+    
+    [Fact]
+    public async Task Import_ValidRequest_SendsRequestToThumbnailCreationQueue()
+    {
+        _ = await SendSimpleValidRequest();
+    
+        var thumbnailRequest = _spy.WrittenItems.Single();
+    
+        thumbnailRequest.Bytes
+            .Should()
+            .BeEquivalentTo(TestingConstants.MinimalJpeg, "thumbnails should be made for valid imports");
     }
 
     private async Task<(ImportRequest request, ImportResult Content)> SendSimpleValidRequest()
@@ -118,101 +171,4 @@ public sealed class ImporterTests : IAsyncLifetime
     {
         await _connection.DisposeAsync();
     }
-}
-
-public class ImportEndpointTests(WebApplicationFactory<Program> factory, ITestOutputHelper helper) : EndpointTest(factory, helper)
-{
-    // [Fact]
-    // public async Task Import_ValidRequest_ReturnsSuccessResult()
-    // {
-    //     (var request, var result) = await SendSimpleValidRequest();
-    //
-    //     result.Should().NotBeNull();
-    //     result.ImportId.Should().Be(request.ImportId);
-    //     result.Results.Single().Ok.Should().BeTrue();
-    // }
-    //
-    // [Fact]
-    // public async Task Import_ValidRequest_WritesFileToSubfolder()
-    // {
-    //     _ = await SendSimpleValidRequest();
-    //
-    //     var expectedPath = FileSystem.Path.Join(AppRoot,
-    //         "db",
-    //         "files",
-    //         "f61",
-    //         "61F461B34DCF8D8227A8691A6625444C1E2C793A181C7D0AD5EF8B15D5E6D040.jpg");
-    //
-    //     var file = FileSystem.GetFile(expectedPath);
-    //
-    //     file.Should().NotBeNull();
-    // }
-    //
-    // [Fact]
-    // public async Task Import_ValidRequest_PersistsInfoToDatabase()
-    // {
-    //     _ = await SendSimpleValidRequest();
-    //
-    //     var mapping = Context.Mappings
-    //         .Include(mapping => mapping.Hash)
-    //         .Include(mapping => mapping.Tag)
-    //         .ThenInclude(tag => tag.Namespace)
-    //         .Include(mapping => mapping.Tag)
-    //         .ThenInclude(tag => tag.Subtag)
-    //         .Single();
-    //
-    //     mapping.Tag.Namespace.Value.Should().Be("category", "the namespace should be linked the tag");
-    //     mapping.Tag.Subtag.Value.Should().Be("example", "the subtag should be linked to the tag");
-    //
-    //     var hashed = HashedBytes.FromUnhashed(TestingConstants.MinimalJpeg);
-    //
-    //     mapping.Hash.Hash.Should().BeEquivalentTo(hashed.Bytes, "we should be persisting the hashed bytes");
-    // }
-    //
-    // [Fact]
-    // public async Task Import_ValidRequest_SendsRequestToThumbnailCreationQueue()
-    // {
-    //     _ = await SendSimpleValidRequest();
-    //
-    //     var thumbnailRequest = SpyChannel.WrittenItems.Single();
-    //
-    //     thumbnailRequest.Bytes
-    //         .Should()
-    //         .BeEquivalentTo(TestingConstants.MinimalJpeg, "thumbnails should be made for valid imports");
-    // }
-    //
-    // private async Task<(ImportRequest request, ImportResult Content)> SendSimpleValidRequest()
-    // {
-    //     var mockFile = new MockFileData(TestingConstants.MinimalJpeg);
-    //
-    //     var filepath = FileSystem.Path.Join(AppRoot, "image.jpg");
-    //
-    //     FileSystem.AddFile(filepath, mockFile);
-    //
-    //     var request = BuildRequest(filepath, "category", "example");
-    //
-    //     var response = await Api.ProcessImport(request);
-    //
-    //     return (request, response.Content!);
-    // }
-    //
-    // private static ImportRequest BuildRequest(string source, string @namespace, string subtag)
-    // {
-    //     var tag = new TagModel(Namespace: @namespace, Subtag: subtag);
-    //
-    //     var item = new ImportItem
-    //     {
-    //         Source = new(source),
-    //         Tags = [tag]
-    //     };
-    //
-    //     var request = new ImportRequest
-    //     {
-    //         Items = [item],
-    //         ImportType = ImportType.File,
-    //         DeleteAfterImport = false
-    //     };
-    //
-    //     return request;
-    // }
 }
