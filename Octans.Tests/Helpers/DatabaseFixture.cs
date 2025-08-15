7756f2f3f838e52bc50c9bbb99d7c6b911ed5581
jackkendall <jkendall3096@gmail.com>
2025-08-15 15:42:02 +0100

    Use the DB fixture

--- Octans.Tests/Helpers/DatabaseFixture.cs
+++ Octans.Tests/Helpers/DatabaseFixture.cs
@@ -1,4 +1,6 @@
 using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.DependencyInjection;
+using Octans.Core.Models;
 
 namespace Octans.Tests;
 
@@ -9,6 +11,16 @@ public class DatabaseFixture : IAsyncLifetime
     public async Task InitializeAsync()
     {
         await Connection.OpenAsync();
+    }
+
+    public async Task ResetAsync(IServiceProvider provider)
+    {
+        await using var scope = provider.CreateAsyncScope();
+
+        var db = scope.ServiceProvider.GetRequiredService<ServerDbContext>();
+
+        await db.Database.EnsureDeletedAsync();
+        await db.Database.EnsureCreatedAsync();
     }
 
     public async Task DisposeAsync()
--- Octans.Tests/Importing/ImporterTests.cs
+++ Octans.Tests/Importing/ImporterTests.cs
@@ -4,7 +4,6 @@ using System.Threading.Channels;
 using Octans.Core.Importing;
 using Microsoft.EntityFrameworkCore;
 using FluentAssertions;
-using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Octans.Client;
@@ -15,29 +14,28 @@ using Xunit.Abstractions;
 
 namespace Octans.Tests;
 
-public sealed class ImporterTests : IAsyncLifetime
+public sealed class ImporterTests : IAsyncLifetime, IClassFixture<DatabaseFixture>
 {
+    private readonly DatabaseFixture _databaseFixture;
     private const string AppRoot = "/app";
-    
+
     private readonly IImporter _sut;
-    private readonly SqliteConnection _connection = new("DataSource=:memory:");
 
     private readonly MockFileSystem _fileSystem = new();
     private readonly SpyChannelWriter<ThumbnailCreationRequest> _spy = new();
     private readonly IServiceProvider _provider;
 
-    public ImporterTests(ITestOutputHelper testOutputHelper)
+    public ImporterTests(ITestOutputHelper testOutputHelper, DatabaseFixture databaseFixture)
     {
+        _databaseFixture = databaseFixture;
         var services = new ServiceCollection();
 
         services.AddLogging(s => s.AddProvider(new XUnitLoggerProvider(testOutputHelper)));
         services.AddBusinessServices();
 
-        services.AddDbContext<ServerDbContext>(options =>
-        {
-            options.UseSqlite(_connection);
-        }, optionsLifetime: ServiceLifetime.Singleton);
-        
+        services.AddDbContext<ServerDbContext>(options => { options.UseSqlite(databaseFixture.Connection); },
+            optionsLifetime: ServiceLifetime.Singleton);
+
         services.AddDbContextFactory<ServerDbContext>();
 
         services.AddSingleton<IFileSystem>(_fileSystem);
@@ -47,9 +45,9 @@ public sealed class ImporterTests : IAsyncLifetime
         services.AddHttpClient();
 
         services.Configure<GlobalSettings>(s => s.AppRoot = AppRoot);
-        
+
         _provider = services.BuildServiceProvider();
-        
+
         _sut = _provider.GetRequiredService<IImporter>();
     }
 
@@ -57,61 +55,92 @@ public sealed class ImporterTests : IAsyncLifetime
     public async Task Import_ValidRequest_ReturnsSuccessResult()
     {
         (var request, var result) = await SendSimpleValidRequest();
-    
-        result.Should().NotBeNull();
-        result.ImportId.Should().Be(request.ImportId);
-        result.Results.Single().Ok.Should().BeTrue();
+
+        result
+            .Should()
+            .NotBeNull();
+
+        result
+            .ImportId
+            .Should()
+            .Be(request.ImportId);
+
+        result
+            .Results
+            .Single()
+            .Ok
+            .Should()
+            .BeTrue();
     }
-    
+
     [Fact]
     public async Task Import_ValidRequest_WritesFileToSubfolder()
     {
         _ = await SendSimpleValidRequest();
-    
+
         var expectedPath = _fileSystem.Path.Join(AppRoot,
             "db",
             "files",
             "f61",
             "61F461B34DCF8D8227A8691A6625444C1E2C793A181C7D0AD5EF8B15D5E6D040.jpg");
-    
+
         var file = _fileSystem.GetFile(expectedPath);
-    
-        file.Should().NotBeNull();
+
+        file
+            .Should()
+            .NotBeNull();
     }
-    
+
     [Fact]
     public async Task Import_ValidRequest_PersistsInfoToDatabase()
     {
         _ = await SendSimpleValidRequest();
 
         using var serviceScope = _provider.CreateScope();
-        
+
         var db = serviceScope.ServiceProvider.GetRequiredService<ServerDbContext>();
-        
-        var mapping = db.Mappings
+
+        var mapping = db
+            .Mappings
             .Include(mapping => mapping.Hash)
             .Include(mapping => mapping.Tag)
             .ThenInclude(tag => tag.Namespace)
             .Include(mapping => mapping.Tag)
             .ThenInclude(tag => tag.Subtag)
             .Single();
-    
-        mapping.Tag.Namespace.Value.Should().Be("category", "the namespace should be linked the tag");
-        mapping.Tag.Subtag.Value.Should().Be("example", "the subtag should be linked to the tag");
-    
+
+        mapping
+            .Tag
+            .Namespace
+            .Value
+            .Should()
+            .Be("category", "the namespace should be linked the tag");
+
+        mapping
+            .Tag
+            .Subtag
+            .Value
+            .Should()
+            .Be("example", "the subtag should be linked to the tag");
+
         var hashed = HashedBytes.FromUnhashed(TestingConstants.MinimalJpeg);
-    
-        mapping.Hash.Hash.Should().BeEquivalentTo(hashed.Bytes, "we should be persisting the hashed bytes");
+
+        mapping
+            .Hash
+            .Hash
+            .Should()
+            .BeEquivalentTo(hashed.Bytes, "we should be persisting the hashed bytes");
     }
-    
+
     [Fact]
     public async Task Import_ValidRequest_SendsRequestToThumbnailCreationQueue()
     {
         _ = await SendSimpleValidRequest();
-    
+
         var thumbnailRequest = _spy.WrittenItems.Single();
-    
-        thumbnailRequest.Bytes
+
+        thumbnailRequest
+            .Bytes
             .Should()
             .BeEquivalentTo(TestingConstants.MinimalJpeg, "thumbnails should be made for valid imports");
     }
@@ -119,55 +148,49 @@ public sealed class ImporterTests : IAsyncLifetime
     private async Task<(ImportRequest request, ImportResult Content)> SendSimpleValidRequest()
     {
         var mockFile = new MockFileData(TestingConstants.MinimalJpeg);
-    
+
         var filepath = _fileSystem.Path.Join(AppRoot, "image.jpg");
-    
+
         _fileSystem.AddFile(filepath, mockFile);
-    
+
         var request = BuildRequest(filepath, "category", "example");
-    
+
         var response = await _sut.ProcessImport(request);
-    
+
         return (request, response);
     }
-    
+
     private static ImportRequest BuildRequest(string source, string @namespace, string subtag)
     {
         var tag = new TagModel(Namespace: @namespace, Subtag: subtag);
-    
+
         var item = new ImportItem
         {
             Source = new(source),
             Tags = [tag]
         };
-    
+
         var request = new ImportRequest
         {
             Items = [item],
             ImportType = ImportType.File,
             DeleteAfterImport = false
         };
-    
+
         return request;
     }
 
     public async Task InitializeAsync()
     {
-        await _connection.OpenAsync();
+        await _databaseFixture.ResetAsync(_provider);
 
-        await using var serviceScope = _provider.CreateAsyncScope();
-        
-        var db = serviceScope.ServiceProvider.GetRequiredService<ServerDbContext>();
-        
-        await db.Database.EnsureCreatedAsync();
+        var folders = _provider.GetRequiredService<SubfolderManager>();
 
-        var folders = serviceScope.ServiceProvider.GetRequiredService<SubfolderManager>();
-        
         folders.MakeSubfolders();
     }
 
-    public async Task DisposeAsync()
+    public Task DisposeAsync()
     {
-        await _connection.DisposeAsync();
+        return Task.CompletedTask;
     }
 }
