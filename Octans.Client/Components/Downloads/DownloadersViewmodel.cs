dcc4c3bd7bb1cff7545e52c1ad582e6a3cb41933
jackkendall <jkendall3096@gmail.com>
2025-09-11 20:04:10 +0100

    Move file

--- Octans.Client/Components/Downloads/DownloadersViewmodel.cs
+++ Octans.Client/Components/Downloads/DownloadersViewmodel.cs
@@ -5,19 +5,17 @@ namespace Octans.Client.Components.Downloads;
 
 public class DownloadersViewmodel(DownloaderFactory factory)
 {
-    private readonly DownloaderFactory _factory = factory;
-
     public List<DownloaderMetadata> Downloaders { get; private set; } = [];
 
     public async Task Load()
     {
-        var downloaders = await _factory.GetDownloaders();
+        var downloaders = await factory.GetDownloaders();
         Downloaders = downloaders.Select(d => d.Metadata).ToList();
     }
 
     public async Task Rescan()
     {
-        var downloaders = await _factory.Rescan();
+        var downloaders = await factory.Rescan();
         Downloaders = downloaders.Select(d => d.Metadata).ToList();
     }
 }
