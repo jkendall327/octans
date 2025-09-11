1e108956c3593ca492bf3d440469befa2bd07185
jackkendall <jkendall3096@gmail.com>
2025-09-11 20:12:10 +0100

    Try using non-primary ctor

--- Octans.Client/Components/Downloads/DownloadersViewmodel.cs
+++ Octans.Client/Components/Downloads/DownloadersViewmodel.cs
@@ -2,19 +2,26 @@ using Octans.Core.Downloaders;
 
 namespace Octans.Client.Components.Downloads;
 
-public class DownloadersViewmodel(DownloaderFactory factory)
+public class DownloadersViewmodel
 {
+    private readonly DownloaderFactory _factory;
+
+    public DownloadersViewmodel(DownloaderFactory factory)
+    {
+        _factory = factory;
+    }
+
     public List<DownloaderMetadata> Downloaders { get; private set; } = [];
 
     public async Task Load()
     {
-        var downloaders = await factory.GetDownloaders();
+        var downloaders = await _factory.GetDownloaders();
         Downloaders = downloaders.Select(d => d.Metadata).ToList();
     }
 
     public async Task Rescan()
     {
-        var downloaders = await factory.Rescan();
+        var downloaders = await _factory.Rescan();
         Downloaders = downloaders.Select(d => d.Metadata).ToList();
     }
 }
